4a9a66f646455231285e9cafdfcf53676b70dffa
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-28 20:01:16 +0530

    Add the unique validator.
    - Username, roll number and email must be unique.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- schema/user.js
+++ schema/user.js
@@ -1,8 +1,15 @@
-module.exports = {
-  id: Number,
-  username: String,
-  name: String,
-  email: String,
-  password: String,
-  rollnum: String
-}
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = mongoose.Schema({
+  id: {type: Number, required: true},
+  email: {type: String, required: true, unique: true},
+  username: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  password: {type: String, required: true},
+  rollnum: {type: String, required: true, unique: true}
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = userSchema;
