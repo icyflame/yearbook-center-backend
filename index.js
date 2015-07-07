84970cd4066daaf2a499c9f558b33255acadce3c
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-07 15:23:33 +0530

    Move the userActions to a separate file.
    - Create sub-files in the lib/ folder to ensure that the API stays
      modular.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var restify = require('restify');
 var mongoose = require('mongoose');
-var util = require('util');
-var crypto = require('crypto');
-var isHexdigest = require('is-hexdigest');
+// var util = require('util');
 
 var server = restify.createServer();
 
@@ -31,91 +29,26 @@ server.use(restify.bodyParser());
 mongoose.connect(process.env.MONGOLAB_DB_URL);
 
 var userSchema = require('./schema/user.js');
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema); // eslint-disable-line
 
-server.post('/user', function (req, res) {
-  if (!(req.params.username && req.params.name && req.params.password
-    && req.params.email && req.params.rollnum)) {
-      res.send(400, 'Required fields missing.');
-    } else if (!isHexdigest(req.params.password)) {
-      res.send(400, 'Password is not a valid SHA256 hash');
-    } else {
-      var newUser = new User(req.params);
-      User.count('*', function (err, count) {
-        newUser.num_id = count + 1;
-        newUser.save(function (err) {
-          if (err) {
-            if (err.name === 'ValidationError') {
-              res.send(400, {
-                name: err.name,
-                errors: err.errors
-              });
-            }
-          } else {
-            res.send(201, {
-              'message': 'New user created'
-            });
-          }
-        });
-      });
-    }
-});
+var signUpUser = require('./lib/userActions.js').signUpUser;
+var loginUser = require('./lib/userActions.js').loginUser;
+var getUserDetails = require('./lib/userActions.js').getUserDetails;
 
-server.post('/user/login', function (req, res) {
-  if (!(req.params.username && req.params.password)) {
-    res.send(400, 'Required fields missing.');
-  } else if (!isHexdigest(req.params.password)) {
-    res.send(400, 'Password is not a valid SHA256 hash.');
-  } else {
-    User.findOne({ username: req.params.username }, function (err, user) {
-      if (err) {
-        res.send(503, err)
-      }
+server.post('/user', signUpUser);
 
-      if (!user) {
-        res.send(404, {
-          'message': 'User does not exist.'
-        });
-      }
+server.post('/user/login', loginUser);
 
-      if (user.password === req.params.password) {
-
-        var temp_token = crypto.randomBytes(20).toString('hex');
-        user.session_token = temp_token;
-
-        user.save(function (err) {
-          if (err) {
-            console.error(err);
-            res.send(503, err);
-          } else {
-            res.send(200, {
-              token: temp_token
-            });
-          }
-        });
-      } else {
-        res.send(401, 'Bad Password');
-      }
-    });
-  }
-});
-
-server.get('/user/:id', function (req, res) {
-  User.findOne({ num_id: req.params.id }, function (err, user) {
-    res.json({
-      name: user.name,
-      username: user.username,
-      email: user.email,
-      rollnum: user.rollnum
-    });
-  });
-});
+server.get('/user/:id', getUserDetails);
 
 server.get('/ping', function (req, res) {
-  res.json("Hello! The API is online!");
+  res.json('Hello! The API is online!');
 });
 
 var port = process.env.PORT || 5000;
 server.listen(port, function (err) {
-  console.log("Started listeing on port " + port);
+  if (err) {
+    throw err;
+  }
+  console.log('Started listeing on port ' + port);
 });
